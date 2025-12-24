0d19067256af4c66c34d5ada2b79a5340b383a0d
mofafe <hf3jd8@gmail.com>
2025-12-25 07:54:01 +0900

    fix: HTML直書きだったJavaScriptをファイルに

--- web/carrot.go
+++ web/carrot.go
@@ -3,7 +3,6 @@ package web
 import (
 	"database/sql"
 	"net/http"
-	"nijitter-client/database"
 
 	"os"
 	"time"
@@ -30,72 +29,35 @@ type ResponseUserData struct {
 }
 
 func TimelineHandle(c *gin.Context) {
-	c.HTML(http.StatusOK, "timeline.html", nil)
+	c.HTML(http.StatusOK, "timeline.html", gin.H{
+		"apiUrl": os.Getenv("API_URL"),
+	})
 }
 
 func UserHandle(c *gin.Context) {
 	userID := c.Param("id")
-
-	row := database.DB.QueryRow(`SELECT username, user_id, bio, status_message, created_at FROM users WHERE user_id = ?`, userID)
-
-	var u userData
-	err := row.Scan(
-		&u.Username,
-		&u.UserID,
-		&u.BIO,
-		&u.StatusMessage,
-		&u.CreatedAt,
-	)
-
-	if err != nil {
-		if err == sql.ErrNoRows {
-			c.JSON(http.StatusNotFound, gin.H{
-				"error": "user not found",
-			})
-			return
-		}
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"error": "サーバー側でエラーが発生しました",
-		})
-		return
-	}
-
-	var iconPath *string
-	if u.IconPath.Valid {
-		iconPath = &u.IconPath.String
-	}
-	var bio *string
-	if u.BIO.Valid {
-		bio = &u.BIO.String
-	}
-	var statusMessage *string
-	if u.StatusMessage.Valid {
-		statusMessage = &u.StatusMessage.String
-	}
-
-	createdAt := u.CreatedAt.Format("2006年1月")
-
 	c.HTML(http.StatusOK, "user.html", gin.H{
-		"username":       u.Username,
-		"userID":         u.UserID,
-		"image_url":      os.Getenv("IMAGE_URL"),
-		"icon_path":      iconPath,
-		"bio":            bio,
-		"status_message": statusMessage,
-		"created_at":     createdAt,
+		"userID":    userID,
+		"image_url": os.Getenv("IMAGE_URL"),
+		"apiUrl":    os.Getenv("API_URL"),
 	})
 }
 
 func CarrotHandle(c *gin.Context) {
 	c.HTML(http.StatusOK, "carrot.html", gin.H{
 		"carrotID": c.Param("id"),
+		"apiUrl":   os.Getenv("API_URL"),
 	})
 }
 
 func NightHandle(c *gin.Context) {
-	c.HTML(http.StatusOK, "night.html", nil)
+	c.HTML(http.StatusOK, "night.html", gin.H{
+		"apiUrl": os.Getenv("API_URL"),
+	})
 }
 
 func PostCarrotHandle(c *gin.Context) {
-	c.HTML(http.StatusOK, "postcarrot.html", nil)
+	c.HTML(http.StatusOK, "postcarrot.html", gin.H{
+		"apiUrl": os.Getenv("API_URL"),
+	})
 }
